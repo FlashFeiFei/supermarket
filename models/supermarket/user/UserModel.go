4f26d079503a91ce24752789475075129f8ec37c
liangyu <liangyu@22.cn>
2018-12-22 12:50:13 +0800

    update

--- models/supermarket/user/UserModel.go
+++ models/supermarket/user/UserModel.go
@@ -1,19 +1,23 @@
 package user
 
-import "github.com/flashfeifei/supermarket/models/supermarket"
+import (
+	"github.com/flashfeifei/supermarket/models/supermarket"
+	"time"
+)
 
+//orm模型
 type User struct {
 	supermarket.Supermarket `orm:"-"`
-	Id                      int    `orm:"pk;auto"`
-	Username                string `orm:"size(32)"`
-	Password                string `orm:"size(40)"`
-	Nickname                string `orm:"size(10)"`
-	Status                  int    `orm:default(1)"`
-	Email                   string `orm:"default(null);size(32)"`
-	Phone                   string `orm:"default(null);size(20)"`
-	Createtime              int    `orm:"auto_now_add"`
-	Updatetime              int    `orm:"auto_now"`
-	Deletetime              int    `orm:"null"`
+	Id                      int       `orm:"pk;auto"`
+	Username                string    `orm:"size(32)"`
+	Password                string    `orm:"size(40)"`
+	Nickname                string    `orm:"size(10)"`
+	Status                  int       `orm:default(1)"`
+	Email                   string    `orm:"default(null);size(32)"`
+	Phone                   string    `orm:"default(null);size(20)"`
+	Createtime              time.Time `orm:"auto_now_add;type(int)"`
+	Updatetime              time.Time `orm:"auto_now;type(int)"`
+	Deletetime              time.Time `orm:"default(0);type(int)"`
 }
 
 func (this *User) TableName() string {
