9167d7f1d0815ef524141f95534801dd3b8cf658
liangyu <liangyu@22.cn>
2018-12-24 20:22:34 +0800

    update

--- controllers/supermarket/user/wechat/MiniprogramUserController.go
+++ controllers/supermarket/user/wechat/MiniprogramUserController.go
@@ -32,12 +32,16 @@ func (this *MiniprogramUserController) Login() {
 	}
 
 	//请求接口成功，异常结果
-	if wx_session["errcode"] != 0 {
-		errcode := wx_session["errcode"].(float64)
-		this.Ctx.Output.JSON(lib.ApiErrOpenPlatform(int(errcode), wx_session), false, false)
-		this.Ctx.Output.Body([]byte(""))
-		return
-	}
+	//if wx_session["errcode"] != 0 {
+	//	errcode := wx_session["errcode"].(float64)
+	//	this.Ctx.Output.JSON(lib.ApiErrOpenPlatform(int(errcode), wx_session), false, false)
+	//	this.Ctx.Output.Body([]byte(""))
+	//	return
+	//}
+
+	wx_session["session_key"] = "session_key123456"
+	wx_session["unionid"] = "unionid123456"
+	wx_session["openid"] = "openid123456"
 
 	//正常结果
 	log.Println(wx_session["session_key"])
@@ -47,7 +51,6 @@ func (this *MiniprogramUserController) Login() {
 	token, err := mini_login_service.Login()
 	if err != nil {
 		beego.Error(err.Error())
-		this.Data["json"] = err.Error()
 		this.Ctx.Output.JSON(lib.ApiErr(wx_session), false, false)
 		this.Ctx.Output.Body([]byte(""))
 		return
--- service/supermarket/user/wechat/MiniprogramLoginService.go
+++ service/supermarket/user/wechat/MiniprogramLoginService.go
@@ -1,10 +1,10 @@
 package wechat
 
 import (
+	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/orm"
 	"github.com/flashfeifei/supermarket/models/supermarket/user"
 	"github.com/flashfeifei/supermarket/models/supermarket/user/wechat"
-	"github.com/pkg/errors"
 	"time"
 )
 
@@ -45,6 +45,8 @@ func (this *miniprogramLoginService) Login() (token *miniprogramLoginToken, err 
 //微信小程序用户注册
 func (this *miniprogramLoginService) register() (miniprogram_user_model *wechat.MiniprogramUserModel, err error) {
 	o := orm.NewOrm()
+	//事务开启
+	o.Begin()
 	//小程序用户模型
 	miniprogram_user_model = new(wechat.MiniprogramUserModel)
 	//查询构建器
@@ -54,48 +56,49 @@ func (this *miniprogramLoginService) register() (miniprogram_user_model *wechat.
 		Filter("deletetime__gt", 0).One(miniprogram_user_model)
 
 	if err == orm.ErrNoRows {
-		o.Begin()
+		beego.Debug("找不到找不到找不到好几次")
 		//找不到记录
 		//注册一下用户
 		spermarket_user := new(user.SupermarketUserModel)
 		qs := o.QueryTable(spermarket_user)
 		account := spermarket_user.CreateUsername()
 		err = qs.Filter("username", account).One(spermarket_user)
-		if spermarket_user != nil {
-			o.Rollback()
-			//注册的账号重了！
-			return nil, errors.New("服务器忙碌，稍后重试！")
-		}
-		//注册supermarket用户
-		spermarket_user.Username = account
-		spermarket_user.Password = "123456"
-		spermarket_user.Nickname = "萌新托尼"
-		spermarket_user.Status = 1
-		spermarket_user.Createtime = time.Now().Unix()
-		spermarket_user.Updatetime = time.Now().Unix()
-		spermarket_user.Deletetime = 0
-		user_id, err := o.Insert(&spermarket_user)
-		if err != nil {
+		if err == orm.ErrNoRows {
+			//注册supermarket用户
+			spermarket_user.Username = account
+			spermarket_user.Password = "123456"
+			spermarket_user.Nickname = "萌新托尼"
+			spermarket_user.Status = 1
+			spermarket_user.Createtime = time.Now().Unix()
+			spermarket_user.Updatetime = time.Now().Unix()
+			spermarket_user.Deletetime = 0
+			user_id, err := o.Insert(spermarket_user)
+			if err != nil {
+				o.Rollback()
+				return nil, err
+			}
+			//注册微信用户，并绑定supermarket用户
+			miniprogram_user_model.AccountType = wechat.USER_TYPE_MINIPROGRAM
+			miniprogram_user_model.Openid = this.openid
+			miniprogram_user_model.Unionid = this.unionid
+			miniprogram_user_model.Createtime = time.Now().Unix()
+			miniprogram_user_model.Updatetime = time.Now().Unix()
+			miniprogram_user_model.Deletetime = 0
+			miniprogram_user_model.UserId = user_id
+			_, err = o.Insert(miniprogram_user_model)
+			if err != nil {
+				//事务回滚
+				o.Rollback()
+				return nil, err
+			}
+			//事务提交
+			o.Commit()
+			return miniprogram_user_model, nil
+		} else {
 			o.Rollback()
 			return nil, err
 		}
-		//注册微信用户，并绑定supermarket用户
-		miniprogram_user_model.AccountType = wechat.USER_TYPE_MINIPROGRAM
-		miniprogram_user_model.Openid = this.openid
-		miniprogram_user_model.Unionid = this.unionid
-		miniprogram_user_model.Createtime = time.Now().Unix()
-		miniprogram_user_model.Updatetime = time.Now().Unix()
-		miniprogram_user_model.Deletetime = 0
-		miniprogram_user_model.UserId = user_id
-		_, err = o.Insert(&miniprogram_user_model)
-		if err != nil {
-			//事务回滚
-			o.Rollback()
-			return nil, err
-		}
-		//事务提交
-		o.Commit()
 	}
-
+	o.Commit()
 	return miniprogram_user_model, nil
 }
