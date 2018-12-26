64493dd2baff519ff997e9de335f85f8d681c3d2
liangyu <liangyu@22.cn>
2018-12-26 15:26:23 +0800

    附件模型

--- models/supermarket/attachment/SupermarketAttachmentModel.go
+++ models/supermarket/attachment/SupermarketAttachmentModel.go
@@ -5,6 +5,14 @@ import "github.com/flashfeifei/supermarket/models/supermarket"
 type SupermarketAttachmentModel struct {
 	supermarket.Supermarket `orm:"-"`
 	Id                      int64 `orm:"pk;auto"`
+	Title                   string
+	Filepath                string
+	Filetype                int64
+	MimeType                string
+	Links                   string
+	Updatetime              int64
+	Createtime              int64
+	Deletetime              int64
 }
 
 func (this *SupermarketAttachmentModel) TableName() string {
