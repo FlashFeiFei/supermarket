d7ddc27ea0b5987855487c6abf0eee580104c933
liangyu <liangyu@22.cn>
2018-12-27 14:22:34 +0800

    image服务

--- service/supermarket/attachment/AttachmentService.go
+++ service/supermarket/attachment/AttachmentService.go
@@ -15,17 +15,17 @@ const (
 	UPLOAD_IMAGE_PATH = "static/upload/"
 )
 
-type attachmentService struct {
+type attachmentImageService struct {
 }
 
-func NewAttachmentService() *attachmentService {
-	return &attachmentService{}
+func NewAttachmentService() *attachmentImageService {
+	return &attachmentImageService{}
 }
 
 //通过上传添加一张图片
 //upload_key是请求协议中，哪个字段保存了图片的信息
 //剩下的都是数据库字段
-func (this *attachmentService) AddImageAttachmentByUpload(upload_key string) (int64, error) {
+func (this *attachmentImageService) AddImageAttachmentByUpload(upload_key string) (int64, error) {
 	beego_context := context.NewContext()
 	//golang上传文件
 	//从网络中读取文件
@@ -65,7 +65,7 @@ func (this *attachmentService) AddImageAttachmentByUpload(upload_key string) (in
 }
 
 //通过外链添加一张图片
-func (this *attachmentService) AddImageAttachmentByLinks(links string) (int64, error) {
+func (this *attachmentImageService) AddImageAttachmentByLinks(links string) (int64, error) {
 	o := orm.NewOrm()
 	attachment_model := new(attachment.SupermarketAttachmentModel)
 	attachment_model.Links = links
@@ -78,4 +78,46 @@ func (this *attachmentService) AddImageAttachmentByLinks(links string) (int64, e
 		return 0, err
 	}
 	return attchment_id, nil
+}
+
+//更新一下属性
+func (this *attachmentImageService) UpdateImageAttachmentInfo(id int64, field map[string]string) (num int64, err error) {
+	o := orm.NewOrm()
+	attachment_model := new(attachment.SupermarketAttachmentModel)
+	attachment_model.Id = id
+	err = o.Read(attachment_model)
+	if err != nil {
+		return 0, err
+	}
+	if value, ok := field["title"]; ok {
+		attachment_model.Title = value
+	}
+	if value, ok := field["links"]; ok {
+		attachment_model.Title = value
+	}
+	attachment_model.Updatetime = time.Now().Unix()
+	num, err = o.Update(attachment_model)
+	if err != nil {
+		return 0, err
+	}
+	return num, nil
+}
+
+//删除一个文件
+func (this *attachmentImageService) DeleteImageAttachment(id int64) (num int64, err error) {
+	o := orm.NewOrm()
+	attachment_model := new(attachment.SupermarketAttachmentModel)
+	attachment_model.Id = id
+	err = o.Read(attachment_model)
+	if err != nil {
+		return 0, err
+	}
+	//删除文件
+	tofile := attachment_model.Filepath
+	os.Remove(tofile)
+	num, err = o.Delete(attachment_model)
+	if err != nil {
+		return 0, err
+	}
+	return num, err
 }
