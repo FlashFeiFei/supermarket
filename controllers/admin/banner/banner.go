24c874502c332b683c6ae7fd69b4f28762be1a33
liangyu <liangyu@22.cn>
2018-12-28 17:05:54 +0800

    banner

--- controllers/admin/banner/banner.go
+++ controllers/admin/banner/banner.go
@@ -4,6 +4,14 @@ type BannerController struct {
 	baseController
 }
 
+func (this *BannerController) Index() {
+	if this.IsAjax() {
+
+	} else {
+		this.Layout = this.GetTemplate() + "/banner/layout.html"
+		this.TplName = this.GetTemplate() + "/banner/banner.html"
+	}
+}
 func (this *BannerController) AddBanner() {
 
 }
